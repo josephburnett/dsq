1b824a8c680decd669db1bef657ef6714a60ef8a
Joseph Burnett <josephburnett79@gmail.com>
2018-09-14 13:30:05 -0400

    Fix main.go.

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Print(b.String())
 	fmt.Printf("Moves: %v\n", b.MoveList())
 	fmt.Printf("Fitness: %v\n", engine.Fitness(b))
-	if m, ok := engine.BestMove(b, types.A); ok {
+	if m, _, ok := engine.BestMove(b, types.A); ok {
 		fmt.Printf("BestMove: %v\n", m)
 	}
 }
